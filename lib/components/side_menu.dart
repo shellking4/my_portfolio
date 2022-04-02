5f3c93243348b47550125e8b8d95c9d56c638bdb
Donald KANTI <donaldkant2000@gmail.com>
2022-04-02 19:18:05 +0100

    committed some changes

--- lib/components/side_menu.dart
+++ lib/components/side_menu.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_portfolio/components/area_info.dart';
@@ -6,11 +8,19 @@ import 'package:my_portfolio/components/knowledges.dart';
 import 'package:my_portfolio/components/my_info.dart';
 import 'package:my_portfolio/components/skills.dart';
 import 'package:my_portfolio/utils/constants.dart';
+import 'dart:html' as html;
+
+import 'package:url_launcher/url_launcher.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({
     Key? key,
   }) : super(key: key);
+
+  static const String github_profile = 'https://github.com/shellking4';
+  static const String linkedin_profile =
+      "https://www.linkedin.com/in/donald-kanti";
+  static const String bitbucket_profile = "https://bitbucket.org/shellking4/";
 
   @override
   Widget build(BuildContext context) {
@@ -33,49 +43,63 @@ class SideMenu extends StatelessWidget {
               Divider(),
               SizedBox(height: defaultPadding / 2),
               TextButton(
-                onPressed: () => {}, 
+                  onPressed: () {
+                    downloadFile("assets/documents/resume.pdf");
+                  },
+                  child: Row(
+                    children: [
+                      Text("DOWNLOAD RESUME",
+                          style: TextStyle(
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .color)),
+                      SizedBox(width: defaultPadding / 2),
+                      SvgPicture.asset("assets/icons/download.svg")
+                    ],
+                  )),
+              Container(
+                margin: EdgeInsets.only(top: defaultPadding),
+                color: Color(0xFF24242E),
                 child: Row(
                   children: [
-                    Text(
-                      "DOWNLOAD RESUME",
-                      style: TextStyle(
-                        color: Theme.of(context).textTheme.bodyText1!.color
-                      )
+                    Spacer(),
+                    IconButton(
+                      onPressed: () {
+                        launchURL(linkedin_profile);
+                      },
+                      icon: SvgPicture.asset("assets/icons/linkedin.svg"),
                     ),
-                    SizedBox(width: defaultPadding / 2),
-                    SvgPicture.asset("assets/icons/download.svg")
+                    IconButton(
+                      onPressed: () {
+                        launchURL(github_profile);
+                      },
+                      icon: SvgPicture.asset("assets/icons/github.svg"),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        launchURL(bitbucket_profile);
+                      },
+                      icon: SvgPicture.asset("assets/icons/bitbucket.svg"),
+                    ),
+                    Spacer(),
                   ],
-                )),
-                Container(
-                      margin: EdgeInsets.only(top: defaultPadding),
-                      color: Color(0xFF24242E),
-                      child: Row(
-                        children: [
-                          Spacer(),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset("assets/icons/linkedin.svg"),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset("assets/icons/github.svg"),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset("assets/icons/twitter.svg"),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                    ),
+                ),
+              ),
             ]),
           ),
         ),
       ]),
     );
   }
+
+  void downloadFile(String url) {
+    html.AnchorElement anchorElement = html.AnchorElement(href: url);
+    anchorElement.download = url;
+    anchorElement.click();
+  }
+
+  void launchURL(String url) async {
+    if (!await launch(url)) throw 'Could not launch $url';
+  }
 }
-
-
-
-
